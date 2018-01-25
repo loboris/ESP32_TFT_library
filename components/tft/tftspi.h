f7147ec9c9f9a5be24f3a1e0b4235f99301fe3cc
0x1abin <270995079@qq.com>
2018-01-25 16:50:55 +0800

    Add support for M5Stack(Custom ILI9341)

--- components/tft/tftspi.h
+++ components/tft/tftspi.h
@@ -94,6 +94,36 @@
 #define PIN_BCKL_OFF 1  	// GPIO value for backlight OFF
 // ---------------------------------------------------------
 
+#elif CONFIG_EXAMPLE_DISPLAY_TYPE == 3
+
+// ** Set the correct configuration for M5Stack TFT
+// ---------------------------------------------------------
+#define DEFAULT_DISP_TYPE   DISP_TYPE_ILI9341
+#define DEFAULT_TFT_DISPLAY_WIDTH   320
+#define DEFAULT_TFT_DISPLAY_HEIGHT  240
+#define DISP_COLOR_BITS_24          0x66
+#define DEFAULT_GAMMA_CURVE         0
+#define DEFAULT_SPI_CLOCK           26000000
+#define TFT_INVERT_ROTATION         0
+#define TFT_INVERT_ROTATION1        0
+#define TFT_INVERT_ROTATION2        1   // Adapte M5Stack TFT
+#define TFT_RGB_BGR                 0x08
+
+#define USE_TOUCH                   TOUCH_TYPE_NONE
+
+#define PIN_NUM_MISO 19		// SPI MISO
+#define PIN_NUM_MOSI 23		// SPI MOSI
+#define PIN_NUM_CLK  18		// SPI CLOCK pin
+#define PIN_NUM_CS   14		// Display CS pin
+#define PIN_NUM_DC   27		// Display command/data pin
+#define PIN_NUM_TCS  0		// Touch screen CS pin (NOT used if USE_TOUCH=0)
+
+#define PIN_NUM_RST  33  	// GPIO used for RESET control (#16)
+#define PIN_NUM_BCKL 32  	// GPIO used for backlight control
+#define PIN_BCKL_ON  1  	// GPIO value for backlight ON
+#define PIN_BCKL_OFF 0  	// GPIO value for backlight OFF
+// ---------------------------------------------------------
+
 #else
 
 // Configuration for other boards, set the correct values for the display used
